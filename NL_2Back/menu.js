6bef2956af8cc926596aee068a025de3629074c0
Raphael Geddert <rmgeddert@gmail.com>
2021-05-17 08:07:08 -0400

    Update menu.js

--- NL_2Back/menu.js
+++ NL_2Back/menu.js
@@ -21,6 +21,11 @@ function gup(name, tmpURL){
 //     return 'Please do not close this window!';
 // }
 
+function startExperiment(){
+  prepareMenu();
+  updateMainMenu(1);
+}
+
 // function for navigating experiment stages
 function updateMainMenu(expStage){
   // update global curstage variable
@@ -75,9 +80,22 @@ $(document).ready(function(){
   $("#NoGo").hide();
 
   // gets MTurk Worker Information and assign to HTML elements
-  document.getElementById('assignmentId').value = gup('assignmentId', document.referrer);
-  document.getElementById('hitId').value = gup('hitId', document.referrer);
-  document.getElementById('workerId').value = gup('workerId', document.referrer);
+  // document.getElementById('assignmentId').value = gup('assignmentId', document.referrer);
+  // document.getElementById('hitId').value = gup('hitId', document.referrer);
+  // document.getElementById('workerId').value = gup('workerId', document.referrer);
+  let mt = getAllUrlParams(window.location.href);
+  // console.log(mt);
+  // if(mt.hitid == ""){mt.hitid = "NA"}
+  // if(mt.workerid == ""){mt.workerid  = "NA"}
+  // if(mt.assid == ""){mt.assid = "NA"}
+
+  document.getElementById('assignmentId').value = mt.assid;
+  document.getElementById('hitId').value = mt.hitid;
+  document.getElementById('workerId').value = mt.workerid;
+
+  console.log(document.getElementById('assignmentId').value);
+  console.log(document.getElementById('hitId').value);
+  console.log(document.getElementById('workerId').value);
 
   // prevents multiple soundcheck checkboxes being selected
   $('input[type="checkbox"]').on('change', function(){
@@ -99,7 +117,7 @@ $(document).ready(function(){
     // prevents previous participants from participating again. See var workerArr above
     if (duplicateWorker(workerId)) {
 
-  		$("#NoGo").html("You have performed our task before. Unfortunately, we are <br/> unable to allow duplicate entries. Please return this HIT. Thanks!")
+  		$("#NoGo").html("You have performed our task before or are otherwise ineligible. Please return this HIT. Thank you for your cooperation, we apologize for any inconvenience.")
   		$("#NoGo").show();
     } else {
     	prepareMenu();
@@ -131,4 +149,70 @@ function prepareMenu(){
       window.alert("Incorrect. Please try again.");
     }
   });
+}
+
+
+function getAllUrlParams(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+    // we'll store the parameters here
+    var obj = {};
+
+    // if query string exists
+    if (queryString) {
+
+        // stuff after # is not part of query string, so get rid of it
+        queryString = queryString.split('#')[0];
+
+        // split our query string into its component parts
+        var arr = queryString.split('&');
+
+        for (var i = 0; i < arr.length; i++) {
+            // separate the keys and the values
+            var a = arr[i].split('=');
+
+            // set parameter name and value (use 'true' if empty)
+            var paramName = a[0];
+            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+            // (optional) keep case consistent
+            paramName = paramName.toLowerCase();
+            if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+            // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+            if (paramName.match(/\[(\d+)?\]$/)) {
+
+                // create key if it doesn't exist
+                var key = paramName.replace(/\[(\d+)?\]/, '');
+                if (!obj[key]) obj[key] = [];
+
+                // if it's an indexed array e.g. colors[2]
+                if (paramName.match(/\[\d+\]$/)) {
+                    // get the index value and add the entry at the appropriate position
+                    var index = /\[(\d+)\]/.exec(paramName)[1];
+                    obj[key][index] = paramValue;
+                } else {
+                    // otherwise add the value to the end of the array
+                    obj[key].push(paramValue);
+                }
+            } else {
+                // we're dealing with a string
+                if (!obj[paramName]) {
+                    // if it doesn't exist, create property
+                    obj[paramName] = paramValue;
+                } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+                    // if property does exist and it's a string, convert it to an array
+                    obj[paramName] = [obj[paramName]];
+                    obj[paramName].push(paramValue);
+                } else {
+                    // otherwise add the property
+                    obj[paramName].push(paramValue);
+                }
+            }
+        }
+    }
+
+    return obj;
 }
