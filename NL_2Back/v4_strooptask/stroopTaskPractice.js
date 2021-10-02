8e47e934e6c8937ef2ab54d0c99f8f582cac97d2
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2021-10-02 14:37:37 -0400

    stroop practice task updates

--- NL_2Back/v4_strooptask/stroopTaskPractice.js
+++ NL_2Back/v4_strooptask/stroopTaskPractice.js
@@ -1,18 +1,61 @@
+let stroopTaskArray;
 function stroopTaskPractice(){
+  sectionType = "pracTask";
+  taskName = "stroopTaskPractice";
+  earlyReleaseExperiment = true;
+  playSoundsExperiment = false;
 
+  currentTaskArray = [];
+
+  $('#instructionsDiv').hide();
+  $("#navButtons").hide();
+  canvas.style.display = "inline-block";
+  $(".canvasas").show();
+
+  stroopTaskArray = createStroopArray(16);
+  console.log(stroopTaskArray);
+  taskFunc = stroopTaskFlow;
+  //countDown(3);
 }
 
-$(document).ready(function(){
-  createStroopArray();
-  canvas = document.getElementById('myCanvas');
-  ctx = canvas.getContext('2d');
-  ctx.textbaseline = "middle";
-  ctx.textAlign = "center";
-  console.log(canvas);
-  console.log(ctx);
-  console.log(ctx.textBaseline);
-  console.log(ctx.textAlign);
-  showInstructions();
+
+function stroopTaskFlow(){
+  if (trialCount <= 16){
+    fixationScreen();
+    setTimeout(stroopTrial, fixInterval);
+  } else {
+    navigateInstructionPath();
+  }
+}
+
+function stroopTrial(){
+  let stimulusText = stroopTaskArray[trialCount - 1][0]
+  let stimulusColor = stroopTaskArray[trialCount - 1][1]
+  let stimulus = stroopTaskArray[trialCount - 1]
+
+  //add stimulus to currentTaskArray (for access by key listener)
+  currentTaskArray.push(stimulus);
+
+  //set up for response
+  stroopOnset = new Date().getTime() - runStart;
+  keyListener = 9, respTime = NaN, partResp = NaN, respOnset = NaN, acc = NaN
+
+  timeoutFunc = practiceTransition;
+  stimTimeout = setTimeout(practiceTransition, stroopStimInterval);
+
+  responseExpected = true;
+  instructionsScreen = false;
+  accuracy = NaN;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "60px Arial";
+  ctx.fillStyle = stimulusColor;
+  ctx.fillText(stimulusText,  canvas.width/2, canvas.height/2);
+  stimOnset = new Date().getTime();
+  respOnset = new Date().getTime();
+  setTimeout(stroopFeedback, stroopITI);
+}
+
+
   $("body").keypress(function(event){
       console.log(event.which);
       responseKey = event.which;
@@ -24,7 +67,6 @@ $(document).ready(function(){
       else {
         if (stimulusColor == "red"){
           if (event.which == 90){
-
           }
           else {
 
@@ -59,77 +101,75 @@ $(document).ready(function(){
         }
 
       }
-  }
-}
+  })
 
+// function stroopFeedback(){
+//   responseExpected = false;
+//   instructionsScreen = false;
+//   ctx.clearRect(0, 0, canvas.width, canvas. height);
+//   ctx.font = "60px Arial";
+//   if (accuracy == 1){
+//     ctx.fillStyle = "green";
+//     ctx.fillText("Correct", canvas.width/2, canvas.height/2);
+//   }
+//   else if (accuracy == 0){
+//     ctx.fillStyle = "red";
+//     ctx.fillText("Incorrect", canvas.width/2, canvas.height/2);
+//   }
+//   else {
+//     ctx.fillStyle = "black";
+//     ctx.fillText("Too slow!", canvas.width/2, canvas.height/2);
+//   }
+//   setTimeout (stroopPracticeITI, 1000);
+// }
 
-function stroopTaskFlow(){
-  if (trialCount <= 16){
-    stroopTrial();
-  }
-  else {
-    navigateInstructionPath();
-  }
-}
+// function stroopPracticeITI(){
+//   instructionsScreen = false;
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   ctx.fillText("", canvas.width/2, canvas.height/2);
+//   reactionTime = (respOnset - stimOnset);
+//   data.push([trialCount, stimulusArray[trialCount], responseKey, accuracy, reactionTime]);
+//   console.log(data);
+//   setTimeout(stroopTaskFlow, 500);
+// }
 
-function stroopTrial(){
-  responseExpected = true;
-  instructionsScreen = false;
-  accuracy = NaN;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.font = "60px Arial";
-  ctx.fillStyle = "black";
-  ctx.fillText(stroopArray[trialCount],  canvas.width/2, canvas.height/2);
-  stimOnset = new Date().getTime();
-  respOnset = new Date().getTime();
-  setTimeout(stroopFeedback, stroopITI);
-}
+function createStroopArray(nTrials){
+  //trial counts
+  let nCongruentTrials = Math.ceil(nTrials/2);
+  let nIncongruentTrials = nTrials - nCongruentTrials;
 
-function stroopFeedback(){
-  responseExpected = false;
-  instructionsScreen = false;
-  ctx.clearRect(0, 0, canvas.width, canvas. height);
-  ctx.font = "60px Arial";
-  if (accuracy == 1){
-    ctx.fillStyle = "green";
-    ctx.fillText("Correct", canvas.width/2, canvas.height/2);
-  }
-  else if (accuracy == 0){
-    ctx.fillStyle = "red";
-    ctx.fillText("Incorrect", canvas.width/2, canvas.height/2);
-  }
-  else {
-    ctx.fillStyle = "black";
-    ctx.fillText("Too slow!"), canvas.width/2, canvas.height/2);
-  }
-  setTimeout (stroopITI, 1000);
+  //create array of 'c's and 'i's, shuffled
+  let congruencyArr = shuffle(new Array(nCongruentTrials).fill("c").concat(new Array(nIncongruentTrials).fill("i")));
 
-function stroopITI(){
-  instructionsScreen = false;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillText("", canvas.width/2, canvas.height/2);
-  reactionTime = (respOnset - stimOnset);
-  data.push([trialCount, stimulusArray[trialCount], responseKey, accuracy, reactionTime]);
-  console.log(data);
-  setTimeout(stroopTaskFlow, 500);
-}
-
-function createStroopArray(){
-  let stimulusSet = ["red", "green", "blue", "orange"]
-    let nCongruentTrials = Math.ceil(nTrials/2)
-    let nIncongruentTrials = nTrials - nCongruentTrials
-    let congruencyArr = shuffle(new Array(nCongruentTrials).fill("c").concat(new Array(nIncongruentTrials).fill("i")))
-
-    let stroopArray = []
-    for (var i = 0; i < congruencyArr.length; i++) {
-      stimulus = _.sample(stimulusSet)
-      if (congruencyArr[i] == "c") {
-        stimulusColor = stimulus;
-        } else { // "i"
-          stimulusColor = _.sample(stimulusSet.filter(color => color != stimulus))
-        }
-        stroopArray.push([stimulus, stimulusColor])
-      }
-      return stroopArray;
+  //create array of stimuli for each trial, based on if trial is a 'c' or 'i' trial
+  let stroopArray = [];
+  for (var i = 0; i < congruencyArr.length; i++){
+    if(congruencyArr[i] == "c"){
+      stroopArray.push(_.sample(congruentStim));
     }
+    else {//"i"
+      stroopArray.push(_.sample(incongruentStim));
+    }
+    return stroopArray;
+  }
+}
+
+function practiceTransition(){
+  keyListener = 0;
+  if (stroopITI > 0){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black"; //accFeedbackColor();
+    ctx.font = "bold 60px Arial";
+    ctx.fillText(accFeedback(),canvas.width/2,canvas.height/2);
+  }
+
+  // log Data
+  // data.push([sectionType, NaN, taskName, NaN, NaN, NaN, trialCount, blockTrialCount, block, fileOnly(activeNode.img.src), NaN, getAccuracy(acc), stimOnset, respOnset, respTime, partResp, activeNode.name, activeNode.index, activeNode.communityNumber, activeNode.community, activeNode.isBoundaryNode ? "b" : "i", NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN]);
+  // console.log(data);
+
+  // iterate trial count
+  trialCount++; blockTrialCount++;
+
+  // return to taskFlow func
+  setTimeout(stroopTaskFlow,stroopITI);
 }
